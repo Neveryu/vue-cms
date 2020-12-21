b26e5e7d77338743a78a14b4cf91edee678256c7
Yu <never_yu@qq.com>
2020-12-21 20:15:07 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import 'normalize.css/normalize.css'
+@import './style/index.css'
+@import './style/custom.css'
+</style>
