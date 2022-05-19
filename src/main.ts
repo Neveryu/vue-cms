842de91cece3af3e0cac9a3f737ec7c58d02405a
Yu(-) <never_yu@qq.com>
2022-05-19 18:34:08 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
 import Antd from 'ant-design-vue'
 import App from './App.vue'
-// 引入less后缀的样式文件，方便后续更改主题颜色
-import 'ant-design-vue/dist/antd.less'
+/**
+ * 引入less后缀的样式文件，方便后续更改主题颜色
+ * 如果你要引入使用less文件的话，按照antd-vue的文档
+ * 你需要使用 modifyVars 的方式来进行覆盖变量（官方文档中提供了webpack,vue-cli2,vue-cli3的示例代码）
+ * vite中类似，主要就是提供一些颜色的变量值
+ * 具体看vite.config.ts中的写法
+ * -----------
+ * Vite 为 Sass 和 Less 改进了 @import 解析，以保证 Vite 别名也能被使用。
+ * 另外，url() 中的相对路径引用的，与根文件不同目录中的 Sass/Less 文件会自动变基以保证正确性。
+ */
+import('ant-design-vue/dist/antd.less')
 
 const app = createApp(App)
 app.use(Antd)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { generateModifyVars } from './build/generate/generateModifyVars'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      // 指定传递给 CSS 预处理器的选项。文件扩展名用作选项的键
+      less: {
+        modifyVars: {
+          'success-color': '#55D187', //  Success color
+          'error-color': '#ED6F6F', //  False color
+          'warning-color': '#EFBD47', //   Warning color
+          'border-color-base': '#EEEEEE',
+          'font-size-base': '14px', //  Main font size
+          'border-radius-base': '2px', //  Component/float fillet
+          'app-content-background': '#fafafa' //   Link color
+        },
+        javascriptEnabled: true
+      }
+    }
+  }
 })
